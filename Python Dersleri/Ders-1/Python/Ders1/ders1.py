9a1b3b55d4c638779077875100f359b11e3385b1
kagandedeturk <55692836+kagandedeturk@users.noreply.github.com>
2021-01-24 11:23:21 +0300

    Add files via upload

---
+++ Python Dersleri/Ders-1/Python/Ders1/ders1.py
@@ -0,0 +1,88 @@
+# Stringler
+
+# Yorum satirlari konusu
+
+"""
+print(
+'Yazar Ahmet'in eserinde 
+"Yanlizlik" vurgusu yapti 
+"Yasam 
+dedi"
+''')
+"""
+
+# input
+
+# print(input("Adinizi giriniz: "))
+
+
+
+# print("Kagan")
+
+# print("Adiniz: " + input("Adinizi giriniz: "))
+
+"""
+print("Grup ismi onerme programi: ")
+
+print("Grup isminiz " + 
+        input("Yasadiginiz sehri giriniz: ") + " "
+         + input("Evcil hayvanizin ismini giriniz: ") 
+         + " olabilir")
+
+"""
+
+# Hata turleri
+
+#print("Deneme)
+
+
+
+
+# pnt(input("Adinizi giriniz: "))
+
+# print(x)
+
+# Variables - Degiskenler
+# 
+
+"""
+isim = "Kagan"
+print(type(isim))
+
+x = 5
+y = 3.5745543
+
+print(type(x))
+print(type(y))
+"""
+
+#Hatali degisken isimleri
+#2isim = kagan
+#sehir ismi = kayseri
+#.ab = 5
+
+#print(2isim)
+#print(sehir ismi)
+
+"""
+#Gecerli degisken isimleri
+kisiIsmi  = "Ahmet"
+sehir_ismi = "Kayseri"
+_2sayi = 5
+
+print(kisiIsmi)
+print(sehir_ismi)
+print(_2sayi)
+"""
+
+sayi1 = 5
+sayi2 = 7.5
+toplam = sayi1 + sayi2
+
+"""
+print("Sayi1 = " + str(sayi1) 
+        + "\nSayi2 = " 
+        + str(sayi2)+"\nToplam = "+ str(toplam))
+"""
+
+print(f"Sayi1 = {sayi1}\nSayi2 = {sayi2}\nToplam = {toplam}")
